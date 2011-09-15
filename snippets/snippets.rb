56db22fcafa5e8d8b209a998ee063346627dbd9a
Kevin Lindsey <klindsey@appcelerator.com>
2011-09-15 13:52:51 -0500

    Remove redundant redefinition (overrides) of the default scope in all snippets. Prevent snippets from being auto-inserted inside JS parameter lists. Note that due to a scope selector limitation, this only prevents auto-insertion for the last parameter in a list.

--- snippets/snippets.rb
+++ snippets/snippets.rb
@@ -1,8 +1,7 @@
-with_defaults :scope => 'source.js' do
+with_defaults :scope => 'source.js -punctuation.definition.parameters.end.js' do
 
 snippet 'Add Events' do |s|
   s.trigger = 'ae'
-  s.scope = 'source.js'
   s.expansion = '${1:this}.addEvents(
     /**
      * @event ${2:eventName}
@@ -15,31 +14,26 @@ end
 
 snippet 'call superclass' do |s|
   s.trigger = 'super'
-  s.scope = 'source.js'
   s.expansion = '${1:MyClassName}.superclass.${2:constructor}.${3:apply}(${4:this, arguments});$0'
 end
 
 snippet 'console.log arguments' do |s|
   s.trigger = 'coa'
-  s.scope = 'source.js'
   s.expansion = 'console.log(arguments);$0'
 end
 
 snippet 'console.log object' do |s|
   s.trigger = 'co'
-  s.scope = 'source.js'
   s.expansion = 'console.log($1);$0'
 end
 
 snippet 'console.log' do |s|
   s.trigger = 'c'
-  s.scope = 'source.js'
   s.expansion = 'console.log(\'$1\');$0'
 end
 
 snippet 'Define Event' do |s|
   s.trigger = 'e'
-  s.scope = 'source.js'
   s.expansion = '/**
  * @event ${1:eventName}
  * ${2:Event description}${3:
@@ -50,7 +44,6 @@ end
 
 snippet 'Ext.apply' do |s|
   s.trigger = 'app'
-  s.scope = 'source.js'
   s.expansion = 'Ext.apply(${1:config}, {
   $0
 });'
@@ -58,7 +51,6 @@ end
 
 snippet 'Ext.applyIf to config' do |s|
   s.trigger = 'aifc'
-  s.scope = 'source.js'
   s.expansion = 'config = config || {};
       
 Ext.applyIf(config, {
@@ -68,7 +60,6 @@ end
 
 snippet 'Ext.applyIf' do |s|
   s.trigger = 'aif'
-  s.scope = 'source.js'
   s.expansion = 'Ext.applyIf(${1:config}, {
   $0
 });'
@@ -76,7 +67,6 @@ end
 
 snippet 'Ext.each' do |s|
   s.trigger = 'each'
-  s.scope = 'source.js'
   s.expansion = 'Ext.each(${1:Array}, ${2:function(${4}) {
   ${0}
 \}}${3:, this});'
@@ -84,7 +74,6 @@ end
 
 snippet 'Ext.extend Object' do |s|
   s.trigger = 'extendo'
-  s.scope = 'source.js'
   s.expansion = '/**
  * @class ${1:MyNamespace.}${2:${TM_FILENAME/(.+)\.js$/$1/}}
  * @extends ${3:Object}
@@ -102,7 +91,6 @@ end
 
 snippet 'Ext.extend' do |s|
   s.trigger = 'extend'
-  s.scope = 'source.js'
   s.expansion = '/**
  * @class ${1:MyNamespace.}${2:${TM_FILENAME/(.+)\.js$/$1/}}
  * @extends ${3:extendsClass}
@@ -124,7 +112,6 @@ end
 
 snippet 'Ext.iterate' do |s|
   s.trigger = 'iterate'
-  s.scope = 'source.js'
   s.expansion = 'Ext.iterate(${1:Object}, ${2:function(key, value) {
   ${0}
 \}}${3:, this});'
@@ -132,13 +119,11 @@ end
 
 snippet 'Ext.ns' do |s|
   s.trigger = 'ns'
-  s.scope = 'source.js'
   s.expansion = 'Ext.ns(\'$1\');$0'
 end
 
 snippet 'Ext.onReady' do |s|
   s.trigger = 'ready'
-  s.scope = 'source.js'
   s.expansion = 'Ext.onReady(function() {
   $0
 });'
@@ -146,7 +131,6 @@ end
 
 snippet 'Ext.override' do |s|
   s.trigger = 'override'
-  s.scope = 'source.js'
   s.expansion = 'Ext.override(${1:Ext.${2:SomeClass}}, {
   $0
 });'
@@ -154,13 +138,11 @@ end
 
 snippet 'Ext.reg' do |s|
   s.trigger = 'reg'
-  s.scope = 'source.js'
   s.expansion = 'Ext.reg(\'${1:xtype}\', ${2:ClassName});$0'
 end
 
 snippet 'Extend Block: config' do |s|
   s.trigger = 'ec'
-  s.scope = 'source.js'
   s.expansion = '/**
  * @cfg {${1:String}} ${2:configName} ${3:Property description}
  */
@@ -169,7 +151,6 @@ end
 
 snippet 'Extend Block: constructor' do |s|
   s.trigger = 'cons'
-  s.scope = 'source.js'
   s.expansion = 'constructor: function(config) {
     config = config || {};
 
@@ -183,7 +164,6 @@ end
 
 snippet 'Extend Block: function' do |s|
   s.trigger = 'ef'
-  s.scope = 'source.js'
   s.expansion = '/**
  * ${10:Function description}${2:
  * @param {${3:String}\} ${4:paramName} ${5:parameter description}}${7:
@@ -196,7 +176,6 @@ end
 
 snippet 'Extend Block: initComponent' do |s|
   s.trigger = 'initc'
-  s.scope = 'source.js'
   s.expansion = 'initComponent: function() {
     $0
   
@@ -206,7 +185,6 @@ end
 
 snippet 'Extend Block: property' do |s|
   s.trigger = 'ep'
-  s.scope = 'source.js'
   s.expansion = '/**
  * ${3:Property description}
  * @property ${1:propertyName}
@@ -217,7 +195,6 @@ end
 
 snippet 'fireEvent (before and after)' do |s|
   s.trigger = 'bfefe'
-  s.scope = 'source.js'
   s.expansion = 'if (${1:this}.fireEvent(\'${3:before-${2:eventName}}\'${4:, ${5:params}}) !== false) {
   $0
   
@@ -227,7 +204,6 @@ end
 
 snippet 'fireEvent (before)' do |s|
   s.trigger = 'bfe'
-  s.scope = 'source.js'
   s.expansion = 'if (${1:this}.fireEvent(\'${2:eventName}\'${3:, ${4:params}})) {
   $0
 }'
@@ -235,13 +211,11 @@ end
 
 snippet 'fireEvent' do |s|
   s.trigger = 'fe'
-  s.scope = 'source.js'
   s.expansion = '${1:this}.fireEvent(\'${2:eventName}\'${3:, ${4:params}});'
 end
 
 snippet 'if not undefined' do |s|
   s.trigger = 'ifnu'
-  s.scope = 'source.js'
   s.expansion = 'if (${1:myObject} != undefined) ${2:{
   $0
 \}}'
@@ -249,7 +223,6 @@ end
 
 snippet 'if undefined' do |s|
   s.trigger = 'ifu'
-  s.scope = 'source.js'
   s.expansion = 'if (${1:myObject} == undefined) ${2:{
   $0
 \}}'
@@ -264,13 +237,11 @@ end
 
 snippet 'JSDoc: class' do |s|
   s.trigger = '* cl'
-  s.scope = 'source.js'
   s.expansion = '* @class ${1:ClassName}'
 end
 
 snippet 'JSDoc: comment block' do |s|
   s.trigger = '/**'
-  s.scope = 'source.js'
   s.expansion = '/**
  * $0
  */'
@@ -278,31 +249,26 @@ end
 
 snippet 'JSDoc: config' do |s|
   s.trigger = '* c'
-  s.scope = 'source.js'
   s.expansion = '* @cfg {${1:String}} ${2:configName} ${3:Config description}'
 end
 
 snippet 'JSDoc: extends' do |s|
   s.trigger = '* e'
-  s.scope = 'source.js'
   s.expansion = '* @extends ${1:ClassName}'
 end
 
 snippet 'JSDoc: param (config)' do |s|
   s.trigger = '* pc'
-  s.scope = 'source.js'
   s.expansion = '* @param {Object} ${1:config} ${2:Optional config object}$0'
 end
 
 snippet 'JSDoc: param' do |s|
   s.trigger = '* p'
-  s.scope = 'source.js'
   s.expansion = '* @param {${1:String}} ${2:paramName} ${3:Parameter description}'
 end
 
 snippet 'JSDoc: property' do |s|
   s.trigger = 'prop'
-  s.scope = 'source.js'
   s.expansion = '/**
  * @property ${1:propertyName}
  * @type ${2:Boolean}
@@ -315,13 +281,11 @@ end
 
 snippet 'JSDoc: return' do |s|
   s.trigger = '* r'
-  s.scope = 'source.js'
   s.expansion = '* @return {${1:String}} ${2:Returned parameter description}'
 end
 
 snippet 'on (addListener)' do |s|
   s.trigger = 'on'
-  s.scope = 'source.js'
   s.expansion = '${1:this}.on(\'${2:click}\', ${4:function($5) {$0\}}, ${3:this});'
 end
 
